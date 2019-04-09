0a238e92934147d5a0ec07b00b8c5415b451365b
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-04-09 16:39:16 +0100

    replace files

--- 01-try catch/index.js
+++ 01-try catch/index.js
@@ -1,51 +1,34 @@
 
-let re;
+const user = {
+  email: 'hi@bolajiayodeji.com'
+};
 
-re = /hello/;
-re = /hello/i; // i flag == case insensitive
-// re = /hello/g; // Global flag == Search entire content
+try{
+  // // Reference error
+  // myFunction();
 
-console.log(re);
-console.log(re.source);
+  // // Type error
+  // null.myFunction();
 
-// exec() - Return results in an array or null
-const results = re.exec('hello world');
-const results2 = re.exec('hi world')
+  // // Syntax error
+  // eval('Hello world');
 
-console.log(results);
-//["hello", index: 0, input: "hello world", groups: undefined]
+  // // URL error
+  // decodeURIComponent('%');
 
-console.log(results2);
-//null
+  if(!user.name) {
+    // throw 'User has no name!';
+    throw new SyntaxError('User has no name!');
+  }
 
-console.log(results[0]);
-console.log(results.index);
-console.log(results.input);
+} catch(e) {
+  console.log(`User Error: ${e.message}`);
+  // console.log(e);
+  // console.log(e.message);
+  // console.log(e.name);
+  // console.log(e instanceof TypeError);
+} finally {
+  console.log('Finally runs regardless of the result...')
+}
 
-// test() - Returns true or false
-const results3 = re.test('Hello');
-const results4 = re.test('hello');
-
-console.log(results3);
-//false
-
-console.log(results4);
-//true
-
-// match() - Return result array or null
-const str = 'Hello there';
-const result = str.match(re);
-
-console.log(result)
-
-// search() - Return index of the first match, else returns -1
-const str2 = 'Bolaji Hello there';
-const result2 = str2.search(re);
-
-console.log(result2);
-
-// replace() - Returna new string with some/or matches of a pattern
-const str3 = 'Hello there';
-const newStr = str.replace(re, 'hi');
-
-console.log(newStr);
+console.log('Program continues...')
